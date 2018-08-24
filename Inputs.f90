dc58c5076c639268abd1bffa05856057d4e38e59
djallum <36492194+djallum@users.noreply.github.com>
2018-08-24 16:13:35 -0400

    Add files via upload

---
+++ Inputs.f90
@@ -0,0 +1,75 @@
+! MODULE INPUTS
+!
+! This module contains all of the input variables. In principle, only this file should be adjusted for new runs, also occasionally the calling program as well. The other pieces of code should only be opened to add new features or to fix bugs.
+!
+! ---------------------------
+! Inputs :
+! ---------------------------
+! PHYSICAL PARAMETERS:
+!      systemn : Number of systems in the ensemble
+!      dim : Number of sites in each system
+!      DELTA : Disorder strength
+!      hop : hopping amplitude
+!      uSite: Interaction Strength
+!      ChemPot : Chemical potential
+! CODING PARAMETERS:
+!      bond_cutoff : Defines the bond strength seperating weak bonds and strong bonds
+!!      bins : number of elements in a number of arrays containing output data (aka number of bins in histograms)
+!      ClusterMax : The maximum cluster size to be included in calculating the 'DOS' or 'Potential'
+!      DOS : Density of States Array. Element i corresponds to the total contributions with energy
+!            in the range [min+(max-min)*i/bins,min+(max-min)*(i+1)/bins]
+!      DOS_EMax : Maximum energy of a DOS contribution allowed
+!      DOS_EMin : Minimum energy of a DOS contribution allowed
+!      
+
+
+module inputs
+  implicit none
+  
+  ! PHYSICAL PARAMETERS
+  integer, parameter :: systemn = 100000  
+  integer, parameter :: dim = 1000
+  real, parameter :: DELTA = 50
+  real, parameter  :: hop = -1
+  real, parameter :: uSite = 20
+  real, parameter :: ChemPot = uSite/2
+  
+  ! CODING PARAMETERS
+  real, parameter :: bond_cutoff = 0.2
+  real, parameter :: prune_cutoff = 3
+  integer, parameter :: bins = 1000
+  integer, parameter :: n_d = systemn*dim
+  integer, parameter :: ClusterMax = dim
+  integer, parameter :: DOS_MaxCluster = 1!ClusterMax
+  real DOS(DOS_MaxCluster,bins)
+  real, parameter :: DOS_EMax = 2*uSite+1, DOS_EMin = -DOS_EMax
+  !real :: DOS_EMax, DOS_EMin
+  real :: DroppedDos !Weight of DOS contributions dropped, might be better to track the weights that have been dropped.
+  integer :: PrunedBonds !# of bonds which are ignored due to the ignore_cutoff'
+  integer :: NumStrongBonds !# of strong bonds to compare with the number of strong bonds that are ignored
+  integer :: StrongestBondsPruned !# of bonds that were pruned where a strong bond was replaced with a weak bond
+  integer, parameter :: Pot_ClSize = 8
+  real Potential(bins)
+  real, parameter :: POT_EMax = Delta/2, POT_EMin = -Pot_EMax
+  real, parameter :: Bond_EMax = 20, Bond_EMin = -20
+  real, parameter :: Diff_EMax = 3*DELTA, Diff_EMin = -3*DELTA
+  real, parameter :: Avg_EMax = DELTA+Usite, Avg_EMin = -Avg_EMax
+  real AvgEDist(bins)
+  !real :: POT_EMax, POT_EMin, Bond_EMax, Bond_EMin, Diff_EMax, Diff_EMin, Avg_EMax, Avg_EMin
+  ! RUN THIS PART?
+  logical, private, parameter :: ff = .false., tt = .true.
+  logical, parameter :: CalcFracSites = tt ! 
+  logical, parameter :: CalcBondStrength = ff
+  logical, parameter :: CalcEnergyDiff = ff
+  logical, parameter :: CalcDos = ff
+  logical, parameter :: CalcPot = ff
+  logical, parameter :: CalcAvgEofB = ff
+
+  real :: times
+
+
+  
+
+end module inputs
+
+  
---
+++ Tools.f90
@@ -0,0 +1,256 @@
+module Tools
+  USE Inputs
+  implicit none
+  SAVE
+
+contains
+  
+  !     Bin_Data subroutine enables the binning of data into a histogram
+  !
+  !
+  !     ------------------------------Arguments-------------------------------------
+  !
+  !     HistoData: A 1xN array, where N is the number of bins in the histogram. This contains the histogram
+  !
+  !     Data: A 1xM array, where M is the number of contributions to be added to HistoData. Each element is the value of a contribution
+  !
+  !     Weights: A 1xM array containing the weights of each contribution in Data
+  !              This argument is optional
+  !
+  !     Dropped: A variable of type integer that keeps track of the number of contributions outside of a specified range
+  !              This argument is optional
+  !
+  !     Max/Min: The maximum (Max) and minimum (Min) values to be counted. Contributions outside are not counted
+  subroutine Bin_Data( HistoData, Data, Weights, Dropped, Max, Min )
+    implicit none
+    real, dimension(:), intent(in) :: Data
+    real, dimension(:), Optional, intent(in) :: Weights
+    real, intent(inout) :: HistoData(bins)
+    real, intent(in) :: Max
+    real, intent(in) :: Min
+    real, optional, intent(inout) :: Dropped
+    integer Loop1, Loop2
+ 
+   
+    If ( Present(Dropped) .and. Present(Weights) ) then
+       
+       do Loop1 = 1,size(Data)
+          if ( Weights(Loop1) .eq. 0 ) CYCLE 
+          if ( ( Data(Loop1) .lt. Min ) .or. ( Data(Loop1) .gt. Max ) ) then
+             Dropped = Dropped + Weights(Loop1)
+          else
+             Loop2 = FLOOR(bins*(Data(Loop1) - Min)/(Max - Min) + 1)
+             if ( (Loop2 .gt. bins) .or. (Loop2 .lt. 1) ) CYCLE             
+             HistoData(Loop2) = HistoData(Loop2) + Weights(Loop1)  
+             
+          end if
+          
+       end do
+    
+    else if ( Present(Dropped) .and. (.not. Present(Weights) ) ) then
+
+       do Loop1 = 1,size(Data) 
+          if ( ( Data(Loop1) .lt. Min ) .or. ( Data(Loop1) .gt. Max ) ) then
+             Dropped = Dropped + Weights(Loop1)
+          else             
+             Loop2 = FLOOR(bins*(Data(Loop1) - Min)/(Max - Min) + 1)
+             if ( (Loop2 .gt. bins) .or. (Loop2 .lt. 1) ) CYCLE
+             HistoData(Loop2) = HistoData(Loop2) + 1
+             
+          end if
+          
+       end do
+
+    else if ( (.not. Present(Dropped)) .and. Present(Weights) ) then
+
+       do Loop1 = 1,size(Data) 
+          if ( ( Data(Loop1) .lt. Min ) .or. ( Data(Loop1) .gt. Max ) ) CYCLE
+          Loop2 = FLOOR(bins*(Data(Loop1) - Min)/(Max - Min) + 1)
+          if ( (Loop2 .gt. bins) .or. (Loop2 .lt. 1) ) CYCLE
+          HistoData(Loop2) = HistoData(Loop2) + Weights(Loop1)
+             
+       end do
+          
+          
+
+    else if ( .not. ( Present(Dropped) .or. Present(Weights) ) ) then
+
+       do Loop1 = 1,size(Data) 
+          if ( ( Data(Loop1) .lt. Min ) .or. ( Data(Loop1) .gt. Max ) ) CYCLE
+          Loop2 = FLOOR(bins*(Data(Loop1) - Min)/(Max - Min) + 1)
+          if ( (Loop2 .gt. bins) .or. (Loop2 .lt. 1) ) CYCLE   
+          HistoData(Loop2) = HistoData(Loop2) + 1
+          
+       end do
+
+    else
+       print*, "Bin_Data options exceeded"
+       STOP
+    end if
+    
+     
+
+  end subroutine Bin_Data
+
+  subroutine OpenFile( Unum, Name, Contents, Column1, Column2 )
+    implicit none
+    integer, intent(in) :: Unum
+    character(len=*), intent(in) :: Name, Contents, Column1, Column2
+    character(len=25) :: filename
+    filename = Name
+    filename = FileNamer(filename)
+    open (unit = Unum, file = "output/"//trim(filename)//"" ,status ='unknown' )
+    
+    
+    write(Unum,*) "#This file contains the following data type for the ensemble: ", Trim(Contents)
+    write(Unum,*) "#First column is: ", Trim(Column1)
+    write(Unum,*) "#Second column is: ", Trim(Column2)
+    write(Unum,*) "#Disorder Strength = ", DELTA
+    write(Unum,*) "#Bond Cutoff = ", bond_cutoff
+    write(Unum,*) "#Pruned bonds cutoff = ", prune_cutoff
+    write(Unum,*) "#Fraction of n.n. sites in which at least one possible bond (of three) was pruned = ", &
+         real(PrunedBonds)/real(n_d)
+    write(Unum,*) "#Fraction of prunings that are useful = ", real(StrongestBondsPruned)/real(PrunedBonds)
+    write(Unum,*) "#Hopping = ", hop
+    write(Unum,*) "#Interaction strength = ", uSite
+    write(Unum,*) "#Chemical Potential = ", ChemPot
+    write(Unum,*) "#Dimensions = ", dim
+    write(Unum,*) "#Number of systems = ", systemn
+
+  end subroutine OpenFile
+  
+  !
+  ! Prints data to file with unit number, Unum. Inputs: Unum = unit number for file, Form = format for the data, Bins array: xaxis, Data = Data.
+  ! DoSMin + (DoSMax-DoSMin)*iii/real(bins)
+  
+  subroutine PrintData( Unum, Form, Min, Max, BinNum, Data, Dropped )
+    implicit none
+    integer, intent(in) :: Unum, BinNum
+    character(len=*), intent(in) :: Form
+    real, intent(in), dimension(:) :: Data
+    real, intent(in) :: Min, Max
+    real, optional, intent(in) :: Dropped
+    real :: BinWidth
+    integer :: Loop1
+
+    If ( Present(Dropped) ) then
+       BinWidth = (Max - Min)/real(BinNum)
+       do Loop1=1,BinNum
+          write(Unum,( Form)) Min + ( Max - Min )*Loop1/real(BinNum) - BinWidth/2, Data(Loop1)/( (Sum(Data) + Dropped) * BinWidth )
+       end do
+    else
+    
+       BinWidth = (Max - Min)/real(BinNum)               ! Area of each bin (bins have equal width)
+       do Loop1=1,BinNum
+          write(Unum,( Form)) Min + ( Max - Min )*Loop1/real(BinNum) - BinWidth/2, Data(Loop1)/( BinWidth )
+       end do
+    end If
+    
+    Close(Unum)
+
+  end subroutine PrintData
+
+  subroutine resize_array(array, numRemove, Start)
+    real, dimension(:), allocatable :: tmp_arr
+    real, dimension(:), allocatable, intent(inout) :: array
+    integer, intent(in) :: numRemove                           ! Number of elements to remove from 'array'
+    integer, intent(in) :: Start                               ! The element in which to start removing the array
+    integer i, j                                               ! Looping integer
+    
+
+    allocate(tmp_arr(size(array) - numRemove))
+
+    tmp_arr(:) = 0
+
+    j=1
+
+    if ( (Start + numRemove) .le. size(array) ) then
+
+
+       do i = 1, Start - 1
+
+          tmp_arr(i) = array(i)
+          j = j + 1
+
+       end do
+
+
+       do i = (Start + numRemove), Size(array)
+
+          tmp_arr(j) = array(i)
+          j = j + 1
+
+       end do
+
+
+
+    else if ( (Start + numRemove) .gt. size(array) ) then
+
+       do i = (Start + numRemove - size(array)) , Start-1
+
+          tmp_arr(j) = array(i)
+          j = j + 1
+
+       end do
+
+
+    end if
+
+    deallocate(array)
+    allocate(array(size(tmp_arr)))
+    
+    array = tmp_arr
+    
+   
+  end subroutine resize_array
+  
+  character(len=20) function str(k)                  ! function to change an integer to a string, used in FileNamer subroutine
+     integer, intent(in) :: k
+
+     write (str, *) k
+     str = adjustl(str)
+   end function str
+
+   character(len=25) function FileNamer(Name)                                ! Increments filename labels Blah023.dat label:023
+     character(len=25), intent(in) :: Name                                   ! Initial filename. Would be 'Blah' from above example
+     integer num                                                             ! Keeps track of the label increment as this continues to check for name existence
+     logical Exist                                                           ! Used to store if a filename exists
+
+     Exist = .true.
+     num = 0
+     FileNamer = ""//trim(Name)//"000.text"                                  ! Takes Blah and puts it into the format 'Blah000.text'
+  
+     do while (Exist)                                                        ! Continue to change filename if the current one exists
+        inquire( file = "output/"//trim(FileNamer)//"", exist = Exist )                           ! Checks the current folder (where this .f90 file is found) if the current filename exists
+        if ( Exist ) then                                                    ! If the file name exists
+           num = num + 1                                                     ! Increment the label
+
+           if ( len(trim(str(num))) .eq. 1 ) then                            ! Labels can be from 000 to 999. To preserve filename length the leading zeros are required
+              FileNamer = ""//trim(Name)//"00"//trim(str(num))//".text"      ! This if statement (and the one below) check the length of the label (1 has length 1, 23 has length 2, 450 has length 3)
+           else if ( len(trim(str(num))) .eq. 2) then                        ! This determines the number of leading zeros to add to the filename label
+              FileNamer = ""//trim(Name)//"0"//trim(str(num))//".text"
+           else
+              FileNamer = ""//trim(Name)//trim(str(num))//".text"
+           end if
+           
+        else if ( .not. Exist ) then                                         ! If it does not exist. Note: Didn't increment 'num'
+           if ( len(trim(str(num))) .eq. 1 ) then
+              FileNamer = ""//trim(Name)//"00"//trim(str(num))//".text"
+           else if ( len(trim(str(num))) .eq. 2) then
+              FileNamer = ""//trim(Name)//"0"//trim(str(num))//".text"
+           else
+              FileNamer = ""//trim(Name)//trim(str(num))//".text"
+           end if
+           Exist = .false.
+        else
+           print*, "You belong in a museum. Error: FileNameErr"              ! Outputs error if something strange happens. Can't imagine what
+        end if
+     end do
+   end function FileNamer
+
+ 
+
+ end module Tools
+ 
+ 
+ 
